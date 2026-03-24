a8d82b1d0902d109a4b9fc9ddbccbf29bf64978f
akshajs <akshajs@student.ubc.ca>
2026-03-24 14:02:31 -0700

    feat: configure server settings and update preview script in astro.config and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,19 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3001,
+  },
+  vite: {
+    server: {
+      hmr: {
+        host: '0.0.0.0',
+      },
+      allowedHosts: [
+        'elwood-unimagined-tasha.ngrok-free.dev',
+        'localhost',
+      ]
+    }
+  }
+});
