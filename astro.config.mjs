df271f327a6c7394a85d96130c37694d179d6191
akshaj-24 <akshaj243@gmail.com>
2026-08-10 19:08:48 -0700

    Astro config url added

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
         'elwood-unimagined-tasha.ngrok-free.dev',
         'localhost',
         'chbe-site.akshajs.org',
+        'ubcchbecouncil.com',
       ]
     }
   }
